749ad7beb8027fd351f53cac0dabf0256d6fbc52
unknown <farhatsnigdho1392@gmail.com>
2024-03-05 18:10:56 +0600

    first commit

---
+++ child-service/src/main/java/com/example/childservice/ChildServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.childservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildServiceApplication.class, args);
+	}
+
+}
